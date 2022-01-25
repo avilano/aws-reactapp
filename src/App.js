a33a5d257db426b755ab71aa4b10ae3b4aa71b9f
Avilano <vira0092@gmail.com>
2022-01-24 23:14:13 -0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
 
-function App() {
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <main>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Hello {user.username} we now have Auth!</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        </div>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
